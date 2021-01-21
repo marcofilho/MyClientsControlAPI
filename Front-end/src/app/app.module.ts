ee8b33425bc8b1a6b7f38b1f0b92f5762d29637d
Marco Antonio Filho <m.barceloslima@gmail.com>
2021-01-21 11:09:53 -0300

    Front-end
    Add front-end project

---
+++ Front-end/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavigationModule } from './navigation/navigation.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NavigationModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
